5234d417a95c4c5c072139188fd4802b8f9eba5e
Amrit <amritsinghal2211@gmail.com>
2024-04-23 15:59:56 +0530

    signup route

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,9 @@ import dotenv from "dotenv"
 import authroutes from "./routes/auth.route.js"
 import connectToMongoDB from "./db/connectToDatabse.js";
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
-app.get("/" ,(req ,res)=>{
-    res.send("Hello World");
-})
+dotenv.config();
+app.use(express.json());
 app.use("/api/auth" ,authroutes);
 app.listen(PORT ,()=>{
     connectToMongoDB();
