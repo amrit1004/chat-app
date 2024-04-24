f44e847ee4b3f75bc389027a492e3e13717c4a0c
Amrit <amritsinghal2211@gmail.com>
2024-04-24 15:57:11 +0530

    added authentication

---
+++ backend/models/message.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const mesageSchema = new mongoose.Schema({
+    senderId:{
+        type: mongoose.Schema.Types.ObjectId ,
+        ref: "User" ,
+        required : true
+    } ,
+    receiverId:{
+        type: mongoose.Schema.Types.ObjectId ,
+        ref: "User" ,
+        required : true
+    } ,
+    message:{
+        type: String ,
+        required: true
+    }
+} ,{timestamps:true}); // createdAt ,updatedAt , message.createdAt :
+const Message = mongoose.model("Message" ,messageSchema);
+export default Message;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv"
 import authroutes from "./routes/auth.route.js"
+import messageroutes from "./routes/message.route.js"
 import connectToMongoDB from "./db/connectToDatabse.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
 app.use("/api/auth" ,authroutes);
+app.use("/api/messages" ,messageroutes);
 app.listen(PORT ,()=>{
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`)}); 
