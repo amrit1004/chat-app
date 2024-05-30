4e8839162972bcab4142c460414225b612bee1a0
Amrit <amritsinghal2211@gmail.com>
2024-05-30 16:37:48 +0530

    added url for live messaging

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -11,7 +11,7 @@ export const SocketContextProvider = ({children})=>{
     const {authUser}  = useAuthContext();
     useEffect (()=>{
       if(authUser){
-        const socket = io("http://localhost:5000" ,{
+        const socket = io("https://chat-app-xnkn.onrender.com" ,{
             query:{
                 userId: authUser._id,
             }
